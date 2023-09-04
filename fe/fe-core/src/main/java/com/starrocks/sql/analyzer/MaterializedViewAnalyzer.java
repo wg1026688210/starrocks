061bf58d5c20c0e7b3333b12d5221f13808c8548
wgcn <1026688210@qq.com>
2023-09-04 17:05:48 +0800

    add paimon mv logic
    Signed-off-by: wgcn <1026688210@qq.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/analyzer/MaterializedViewAnalyzer.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/analyzer/MaterializedViewAnalyzer.java
@@ -154,11 +154,11 @@ public class MaterializedViewAnalyzer {
                 continue;
             }
 
-            if (!FeConstants.isReplayFromQueryDump && isExternalTableFromResource(table)) {
-                throw new SemanticException(
-                        "Only supports creating materialized views based on the external table " +
-                                "which created by catalog", tableNameInfo.getPos());
-            }
+//            if (!FeConstants.isReplayFromQueryDump && isExternalTableFromResource(table)) {
+//                throw new SemanticException(
+//                        "Only supports creating materialized views based on the external table " +
+//                                "which created by catalog", tableNameInfo.getPos());
+//            }
             baseTableInfos.add(BaseTableInfo.fromTableName(tableNameInfo, table));
         }
         processViews(queryStatement, baseTableInfos, withCheck);
