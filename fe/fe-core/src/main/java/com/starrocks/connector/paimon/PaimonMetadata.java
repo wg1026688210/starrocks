08952c3753b79c7dc80ff9a5a4d4f6d1a5a342a4
wgcn <1026688210@qq.com>
2023-09-17 21:01:55 +0800

    Revert "add paimon mv logic"
    This reverts commit 26eb2e6e341236cba3d54d6ae7c8a99d4b067acd.

--- fe/fe-core/src/main/java/com/starrocks/connector/paimon/PaimonMetadata.java
+++ fe/fe-core/src/main/java/com/starrocks/connector/paimon/PaimonMetadata.java
@@ -46,7 +46,6 @@ import org.apache.paimon.table.source.DataSplit;
 import org.apache.paimon.table.source.ReadBuilder;
 import org.apache.paimon.table.source.Split;
 import org.apache.paimon.types.DataField;
-import org.apache.paimon.types.DataType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -154,9 +153,8 @@ public class PaimonMetadata implements ConnectorMetadata {
         ArrayList<Column> fullSchema = new ArrayList<>(fields.size());
         for (DataField field : fields) {
             String fieldName = field.name();
-            DataType type = field.type();
-            Type fieldType = ColumnTypeConverter.fromPaimonType(type);
-            Column column = new Column(fieldName, fieldType, type.isNullable());
+            Type fieldType = ColumnTypeConverter.fromPaimonType(field.type());
+            Column column = new Column(fieldName, fieldType, true);
             fullSchema.add(column);
         }
         PaimonTable table = new PaimonTable(catalogName, dbName, tblName, fullSchema,
